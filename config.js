52e05d1f098bd09b50bf65612bd52f2cfdca9322
Lizbethz <lizperezteamo@gmail.com>
2025-07-08 20:31:38 -0600

    Update config.js

--- config.js
+++ config.js
@@ -16,8 +16,8 @@ global.isBaileysFail = true;
 global.defaultLenguaje = 'es';
 
 global.owner = [
-  ['5219996125657', '👑 Propietario 👑', true],
-  ['5492916450307'],
+  ['5215641961070', '👑 Propietario 👑', true],
+  ['595982959395'],
   ['5493794297363'],
   ['59169082575']
 ];
